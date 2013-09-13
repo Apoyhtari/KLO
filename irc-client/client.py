ab9c2941b6a9a3d452aff20ff3bf451166487499
Jukka Pietilä <jukka.pietila@digital-identity.com>
2013-09-13 14:41:05 +0300

    Refactored code

--- irc-client/client.py
+++ irc-client/client.py
@@ -8,34 +8,34 @@ class App:
         w.pack()
         w.create_rectangle(10, 350, 500, 10, fill="white")
 
-        
-root = Tk()
-app = App(root)
-
-# create a menu
-menu = Menu(root)
-root.config(menu=menu)
-
-filemenu = Menu(menu)
-menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="new")
-filemenu.add_command(label="open...")
-filemenu.add_separator()
-filemenu.add_command(label="exit")
 def callback():
-        print "called the callback!"
-e = Entry(root, width=80)
-e.pack()
-e.delete(0, END)
-e.insert(0, "a default value")
-b = Button(root, text="Send", width=10, command=callback)
-b.pack()
+	print "called the callback!"
 
-RTitle=root.title("Windows")
+if __name__ = "__main__":        
+	root = Tk()
+	app = App(root)
 
-root.geometry("600x600+300+300")
+	# create a menu
+	menu = Menu(root)
+	root.config(menu=menu)
 
+	filemenu = Menu(menu)
+	menu.add_cascade(label="File", menu=filemenu)
+	filemenu.add_command(label="new")
+	filemenu.add_command(label="open...")
+	filemenu.add_separator()
+	filemenu.add_command(label="exit")
 
+	e = Entry(root, width=80)
+	e.pack()
+	e.delete(0, END)
+	e.insert(0, "a default value")
+	b = Button(root, text="Send", width=10, command=callback)
+	b.pack()
 
-root.mainloop()
+	RTitle=root.title("Windows")
+
+	root.geometry("600x600+300+300")
+
+	root.mainloop()
 

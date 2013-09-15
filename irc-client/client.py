3692f1f070c52193a873700403b08d0fd7a3dd41
Aleksi pöyhtäri <alpoyhta@paju.oulu.fi>
2013-09-15 20:28:39 +0300

    Slightly modified the entry widget. It now takes the entry as a StringVar. Also canvas displays the StringVar but it is not finished. (Currently it just displays PY_VAR0 and does not update when clicked)

--- irc-client/client.py
+++ irc-client/client.py
@@ -3,16 +3,16 @@ import sys
 
 class App:
     def __init__(self, master):
-
+    
         w = Canvas(master, width=500, height=400)
         w.pack()
         w.create_rectangle(10, 350, 500, 10, fill="white")
         canvas_id = w.create_text(15, 10, anchor="nw")
-        w.itemconfig(canvas_id, text="this is some cool shit")
+        w.itemconfig(canvas_id, text=StringVar())
         w.insert(canvas_id, 12, " ")
         
 def callback():
-	print e.get()
+	print StringVar()
 	
 
 if __name__ == "__main__":        
@@ -29,16 +29,15 @@ if __name__ == "__main__":
 	filemenu.add_command(label="open...")
 	filemenu.add_separator()
 	filemenu.add_command(label="exit")
-
-	e = Entry(root, width=80)
+	
+	v = StringVar()
+	e = Entry(root, width=80, textvariable=v)
 	e.pack()
-	e.delete(0, END)
-	e.insert(0, "a default value")
-	e.focus_set()
+	v.set("a default value")
 	b = Button(root, text="Send", width=10, command=callback)
 	b.pack()
+	s =v.get()
 	
-
 	
 	RTitle=root.title("Windows")
 

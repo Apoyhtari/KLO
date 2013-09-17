bb2b4958c70a64d849d1b7f1d6252a5c6b08eab0
aleksi pöyhtäri <aleksi.poyhtari@gmail.com>
2013-09-17 10:33:23 +0300

    Cleaning up code to make it more readable

--- irc-client/client.py
+++ irc-client/client.py
@@ -9,16 +9,20 @@ class App:
         self.w.pack()
         self.w.create_rectangle(10, 350, 500, 10, fill="white")
         self.canvas_id = self.w.create_text(15, 10, anchor="nw")
+        
         self.e = Entry(root, width=80)
         self.e.pack()
         self.e.delete(0, END)
         self.e.insert(0, "a default value")
         self.s = self.e.get()
+        
         self.b = Button(root, text="Send", width=10, command=self.callback)
         self.b.pack()
+        
         self.w.itemconfig(self.canvas_id, text=self.s)
         self.w.insert(self.canvas_id, 12, "")
         self.w.update()
+        
     def callback(self):
         self.s += "\n" +self.e.get()
         self.w.itemconfig(self.canvas_id, text=self.s)
@@ -40,14 +44,6 @@ if __name__ == "__main__":
 	filemenu.add_command(label="open...")
 	filemenu.add_separator()
 	filemenu.add_command(label="exit")
-	
-	
-	
-	
-
-	
-	       
-	
 	
 	RTitle=root.title("Windows")
 
